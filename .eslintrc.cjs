f555d0bd02e7ec0b7ad9468766ce91922c093e65
Eduardo Castro <edusilva.dev@gmail.com>
2024-03-16 11:27:32 -0300

    setup: start o project e adiciona regras do eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'indent': [
+      'error',
+      'tab'
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-multi-spaces': [
+      'error'
+    ],
+    'no-multiple-empty-lines': [
+      2,
+      { 'max': 1, 'maxBOF': 0, 'maxEOF': 0 }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => {
+	return (
+		<h1>Vite + React</h1>
+	);
+};
