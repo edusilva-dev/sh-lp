a946ea286192bceb7dfac512dea5269d5e65009d
Lucas Rafael Gonçalves <lrafaelgoncalves.lrg@gmail.com>
2024-03-17 19:09:32 -0300

    feat: ajusta mobile de banner de diferenciais exclusivos da tela de candidatos

--- src/pages/candidate/components/exclusive-differences/exclusive-differences.component.tsx
+++ src/pages/candidate/components/exclusive-differences/exclusive-differences.component.tsx
@@ -4,84 +4,106 @@ import VerticalBars from '@/assets/icons/vertical-bars.svg?react';
 import jobGenerateDesktopPath from '@/assets/images/job-generate-desktop.png';
 import { Container } from '@/components/container';
 import { ExclusiveDifferencesCard } from '@/components/exclusive-differences-card';
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Stack, Theme, Typography, useMediaQuery } from '@mui/material';
 import { FC } from 'react';
 
-export const ExclusiveDifferences: FC = () => (
-	<Container>
-		<Stack gap={6}>
-			<Typography variant="h1">Diferenciais Exclusivos</Typography>
-			<Stack direction="row" justifyContent="center" gap={4}>
-				<ExclusiveDifferencesCard flex="0 1 885px" height="500px">
-					<Stack direction="row" height="100%" gap={4}>
+export const ExclusiveDifferences: FC = () => {
+	const isMobile = useMediaQuery<Theme>((theme) =>
+		theme.breakpoints.down('lg')
+	);
+
+	return (
+		<Container>
+			<Stack gap={6}>
+				<Typography variant={isMobile ? 'h2' : 'h1'}>
+					Diferenciais Exclusivos
+				</Typography>
+				<Stack
+					direction={isMobile ? 'column' : 'row'}
+					justifyContent="center"
+					gap={4}
+				>
+					<ExclusiveDifferencesCard
+						flex={isMobile ? 1 : '0 1 885px'}
+						height="500px"
+					>
 						<Stack
-							alignItems="center"
-							justifyContent="center"
-							flex="0 0 378.5px"
-							overflow="hidden"
-							sx={{
-								backgroundImage: `url(${jobGenerateDesktopPath})`,
-								backgroundSize: 'cover',
-							}}
+							direction={isMobile ? 'column' : 'row'}
+							height="100%"
+							gap={4}
 						>
-							<Button
-								startIcon={<Stars />}
-								sx={{ width: '80%' }}
-								color="secondary"
-								variant="contained"
+							<Stack
+								alignItems="center"
+								justifyContent="center"
+								flex="0 0 378.5px"
+								overflow="hidden"
+								sx={{
+									borderRadius: 2,
+									backgroundImage: `url(${jobGenerateDesktopPath})`,
+									backgroundSize: 'cover',
+								}}
 							>
-								Me conte sobre sua experiência.
-							</Button>
-						</Stack>
-						<Stack justifyContent="space-between">
-							<Stack gap={2}>
-								<Typography variant="h2">Skill Hunter IA</Typography>
-								<Typography variant="h2" fontWeight={400}>
-									A inteligência artificial que te ajuda a chegar mais longe,
-									mais rápido
+								<Button
+									startIcon={<Stars />}
+									sx={{ width: '80%' }}
+									color="secondary"
+									variant="contained"
+								>
+									Me conte sobre sua experiência.
+								</Button>
+							</Stack>
+							<Stack justifyContent="space-between" gap={3}>
+								<Stack gap={2}>
+									<Typography variant={isMobile ? 'h3' : 'h2'}>
+										Skill Hunter IA
+									</Typography>
+									<Typography variant={isMobile ? 'h3' : 'h2'} fontWeight={400}>
+										A inteligência artificial que te ajuda a chegar mais longe,
+										mais rápido
+									</Typography>
+								</Stack>
+								<Typography color="#1E2E3A" fontWeight={400}>
+									A Skill Hunter te mostra vagas adequadas para o seu perfil. Ao
+									aplicar para uma vaga, conte brevemente sobre sua experiência
+									e para estar qualificado em poucos passos
 								</Typography>
 							</Stack>
-							<Typography color="#1E2E3A" fontWeight={400}>
-								A Skill Hunter te mostra vagas adequadas para o seu perfil. Ao
-								aplicar para uma vaga, conte brevemente sobre sua experiência e
-								para estar qualificado em poucos passos
-							</Typography>
 						</Stack>
+					</ExclusiveDifferencesCard>
+					<Stack gap={4} flex="0 1 430px">
+						<ExclusiveDifferencesCard flex="1">
+							<Stack gap={4} height="100%" justifyContent="space-between">
+								<VerticalBars />
+								<Stack>
+									<Typography variant="h2">Acompanhe seu desempenho</Typography>
+									<Typography color="#1E2E3A" fontWeight={400}>
+										<Typography fontWeight={500} component="span">
+											Sem processos confusos
+										</Typography>
+										: acompanhe e melhore seu desempenho para se destacar entre
+										os candidatos
+									</Typography>
+								</Stack>
+							</Stack>
+						</ExclusiveDifferencesCard>
+						<ExclusiveDifferencesCard flex="1">
+							<Stack gap={4} height="100%" justifyContent="space-between">
+								<Notebook />
+								<Stack>
+									<Typography variant="h2">Aplicação simplificada</Typography>
+									<Typography color="#1E2E3A" fontWeight={400}>
+										<Typography fontWeight={500} component="span">
+											Responda somente o necessário
+										</Typography>
+										: A Skill Hunter guarda todas as suas informações de forma
+										segura para que você não precise se repetir
+									</Typography>
+								</Stack>
+							</Stack>
+						</ExclusiveDifferencesCard>
 					</Stack>
-				</ExclusiveDifferencesCard>
-				<Stack gap={4} flex="0 1 430px">
-					<ExclusiveDifferencesCard flex="1">
-						<Stack height="100%" justifyContent="space-between">
-							<VerticalBars />
-							<Stack>
-								<Typography variant="h2">Acompanhe seu desempenho</Typography>
-								<Typography color="#1E2E3A" fontWeight={400}>
-									<Typography fontWeight={500} component="span">
-										Sem processos confusos
-									</Typography>
-									: acompanhe e melhore seu desempenho para se destacar entre os
-									candidatos
-								</Typography>
-							</Stack>
-						</Stack>
-					</ExclusiveDifferencesCard>
-					<ExclusiveDifferencesCard flex="1">
-						<Stack height="100%" justifyContent="space-between">
-							<Notebook />
-							<Stack>
-								<Typography variant="h2">Aplicação simplificada</Typography>
-								<Typography color="#1E2E3A" fontWeight={400}>
-									<Typography fontWeight={500} component="span">
-										Responda somente o necessário
-									</Typography>
-									: A Skill Hunter guarda todas as suas informações de forma
-									segura para que você não precise se repetir
-								</Typography>
-							</Stack>
-						</Stack>
-					</ExclusiveDifferencesCard>
 				</Stack>
 			</Stack>
-		</Stack>
-	</Container>
-);
+		</Container>
+	);
+};
--- src/pages/candidate/components/sign-in-section/sign-in-section.component.tsx
+++ src/pages/candidate/components/sign-in-section/sign-in-section.component.tsx
@@ -1,28 +1,50 @@
 import { Container } from '@/components/container';
-import { Button, Link, Stack, Typography } from '@mui/material';
+import {
+	Button,
+	Link,
+	Stack,
+	Theme,
+	Typography,
+	useMediaQuery,
+} from '@mui/material';
 import { FC } from 'react';
 
-export const SignInSection: FC = () => (
-	<Container>
-		<Stack direction="row" alignItems="center" justifyContent="space-between">
-			<Typography variant="h3" fontWeight={500} color="#1E2E3A">
-				Encontre as melhores oportunidades nas maiores empresas do país
-			</Typography>
-			<Stack direction="row" alignItems="center" gap={3}>
-				<Button variant="contained">Cadastre-se</Button>
-				<Typography
-					fontWeight={400}
-					color="primary.main"
-					component={Stack}
-					direction="row"
-					gap={0.5}
-				>
-					Já possui uma conta?
-					<Link href="#" fontWeight={500} sx={{ textDecoration: 'none' }}>
-						Entre
-					</Link>
+export const SignInSection: FC = () => {
+	const isMobile = useMediaQuery<Theme>((theme) =>
+		theme.breakpoints.down('lg')
+	);
+
+	return (
+		<Container>
+			<Stack
+				gap={3}
+				direction={isMobile ? 'column' : 'row'}
+				alignItems="center"
+				justifyContent="space-between"
+			>
+				<Typography variant="h3" fontWeight={500} color="#1E2E3A">
+					Encontre as melhores oportunidades nas maiores empresas do país
 				</Typography>
+				<Stack
+					direction={isMobile ? 'column' : 'row'}
+					alignItems="center"
+					gap={3}
+				>
+					<Button variant="contained">Cadastre-se</Button>
+					<Typography
+						fontWeight={400}
+						color="primary.main"
+						component={Stack}
+						direction="row"
+						gap={0.5}
+					>
+						Já possui uma conta?
+						<Link href="#" fontWeight={500} sx={{ textDecoration: 'none' }}>
+							Entre
+						</Link>
+					</Typography>
+				</Stack>
 			</Stack>
-		</Stack>
-	</Container>
-);
+		</Container>
+	);
+};
