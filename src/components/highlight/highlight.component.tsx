1dfbb34ad52a14e7fc05fc6596bc2f8ca70ebb40
Lucas Rafael Gonçalves <lrafaelgoncalves.lrg@gmail.com>
2024-03-18 14:38:06 -0300

    refactor: reinicia refatoração de highlight

--- src/components/highlight/highlight.component.tsx
+++ src/components/highlight/highlight.component.tsx
@@ -1,21 +1,30 @@
+import highlight from '@/assets/images/highlight.png';
+import { Stack, Typography } from '@mui/material';
 import { FC } from 'react';
-import { Container, HighilightSubtitle, HighlightTitle, ImageContainer, TitlesContainer } from './highlight.styles';
-import highlightMobilePath from '@/assets/images/mobile-highlight.png';
 
 export const Highlight: FC = () => {
 	return (
-		<Container>
-			<TitlesContainer>
-				<HighlightTitle variant='h1'>A melhor plataforma de R&S do Brasil</HighlightTitle>
-
-				<HighilightSubtitle variant='subtitle1'>
-				A Skill Hunter é uma solução completa que faz a ponte entre recrutadores e candidatos qualificados
-				</HighilightSubtitle>
-			</TitlesContainer>
-			
-			<ImageContainer>
-				<img src={highlightMobilePath} className='mobile' />
-			</ImageContainer>
-		</Container>
+		<Stack
+			direction="row"
+			height="calc(100vh - 70px)"
+			bgcolor="primary.main"
+			paddingX={12}
+			sx={(theme) => ({
+				background: `url(${highlight}), ${theme.palette.primary.main}`,
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'auto 120%',
+				backgroundPositionX: 'right',
+			})}
+		>
+			<Stack maxWidth="590px" justifyContent="center" flex={1} gap={3}>
+				<Typography variant="hero" color="secondary.main" fontWeight={700}>
+					A melhor plataforma de R&S do Brasil
+				</Typography>
+				<Typography variant="h2" color="secondary.main" fontWeight={400}>
+					A Skill Hunter é uma solução completa que faz a ponte entre
+					recrutadores e candidatos qualificados
+				</Typography>
+			</Stack>
+		</Stack>
 	);
 };
