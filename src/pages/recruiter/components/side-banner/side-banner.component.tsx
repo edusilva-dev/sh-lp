4cd904d9942f0f85934e200354c28b8299d6c845
Eduardo Castro <edusilva.dev@gmail.com>
2024-03-20 16:08:47 -0300

    refactor(intervies-image): troca imagem

--- src/pages/recruiter/components/side-banner/side-banner.component.tsx
+++ src/pages/recruiter/components/side-banner/side-banner.component.tsx
@@ -1,4 +1,4 @@
-import interview from '@/assets/images/interview.jpg';
+import interview from '@/assets/images/interview.png';
 import { PlansButtons } from '@/components/plans-buttons/plans-buttons.component';
 import { Box, Stack, Theme, Typography, useMediaQuery } from '@mui/material';
 import { FC } from 'react';
